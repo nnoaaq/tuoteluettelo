6f6c840a4def18f2d317394f239da063359200df
Noa Julkunen <s1900877@edu.bc.fi>
2021-05-11 10:56:25 +0300

    lisätty haku ja sort metodit. Myös fontawesome ja bootstrap otettu käyttöön

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
-
+import { Component, Input } from '@angular/core';
+import { faChevronDown } from '@fortawesome/free-solid-svg-icons';
+import puhelinData from 'src/assets/phones/phones.json'
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +8,54 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'tuoteluettelo';
+  suodatusIkoni = faChevronDown;
+  suodatus = '';
+  puhelimet = puhelinData;
+  suodatetut = [];
+  lajitellut = [];
+  haku(event) {
+    const arvo = event.target.value;
+    this.puhelimet = this.suodata(puhelinData, arvo);
+  }
+  newest() {
+    this.puhelimet = this.puhelimet.slice(0).sort(function (a, b) {
+      return a.age - b.age;
+    });
+  }
+
+  alphabetical() {    console.log(this.puhelimet);
+
+    this.puhelimet = this.puhelimet.slice(0).sort(function (a, b) {
+      var x = a.name.toLowerCase();
+      var y = b.name.toLowerCase();
+      return x < y ? -1 : x > y ? 1 : 0;
+    });
+  }
+  suodatin(e) {
+    if (e.target.value === "alphabetical") {
+      this.alphabetical();
+    } else if (e.target.value === "newest") {
+      this.newest();
+   }
+  }
+  yksilo(e) {
+    console.log(e);
+  }
+  suodata(suodatettava, termi) {
+    if (termi) {
+      for (let puhelin of suodatettava) {
+        if (puhelin.name.toUpperCase().includes(termi.toUpperCase())) {
+          if (!this.suodatetut.includes(puhelin)) {
+          this.suodatetut.push(puhelin)
+          } else {           
+            this.suodatetut.splice(0, this.suodatetut.indexOf(puhelin))
+        }
+        }
+        
+}
+return this.suodatetut.sort();
+    } else {
+      return puhelinData;
+    }
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FontAwesomeModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
